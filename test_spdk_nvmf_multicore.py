dd91ac38420dc78dc9faac4a93373bbea8e585e7
liang <yangliang20065613@126.com>
2018-09-07 10:09:47 +0800

    test nvmf script

---
+++ test_spdk_nvmf_multicore.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import sys
+import os
+import commands
+import shutil
+import signal
+import time
+
+
+opt = ['write', 'read', 'randwrite', 'randread']
+
+def main():
+
+    for item_opt in opt:
+        file_name = item_opt + '-64q-8cpu-' + "4cpuclt"
+        cmd = "./perf -q 64 -s 512 -w " + item_opt + " -t 60 -c 0x1e -r 'trtype:RDMA adrfam:IPv4 traddr:192.168.4.38 trsvcid:4420'|tee -a ./result/8cpu/" + file_name
+        os.system(cmd);
+
+if __name__ == '__main__':
+    main()
